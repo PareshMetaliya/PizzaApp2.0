510d77fe1c9bf5c780ec738f0436770d62c1ac9f
unknown <metaliya.paresh@gmail.com>
2025-03-18 21:16:12 +0530

    Update CORS with frontend URL

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ connectDB();
 
 const app = express();
 
-app.use(cors({ origin: "https://pizza-app2-0.vercel.app/", credentials: true }));
+app.use(cors({ origin: "https://pizza-app2-0.vercel.app", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
